471173ba4d63338b810fef79587df8b892495c65
Leo Mendez <leomendez@Leos-MacBook-Pro.local>
2020-05-17 15:14:33 -0500

    Updates

---
+++ precache-manifest.f7ba811f7bb93349bcfcc66cfc52b224.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41e2d63c8a4e1b8eac2ee5f3b3f3a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bbd4b97ae4a65d5e7ce",
+    "url": "/static/css/main.77e537e0.chunk.css"
+  },
+  {
+    "revision": "62edb82b6bc14283dfd9",
+    "url": "/static/js/2.06b66f2c.chunk.js"
+  },
+  {
+    "revision": "d0a833f5214b6b67664262381f680d82",
+    "url": "/static/js/2.06b66f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbd4b97ae4a65d5e7ce",
+    "url": "/static/js/main.7fada024.chunk.js"
+  },
+  {
+    "revision": "bf860afc129a6a6fcc1e",
+    "url": "/static/js/runtime-main.91b7fe6e.js"
+  },
+  {
+    "revision": "ff366e77255a9a78a97f3e5939c416a8",
+    "url": "/static/media/AvenirNext-Bold.ff366e77.ttf"
+  },
+  {
+    "revision": "8ac88e9179e2a8605cabac45dd86bbec",
+    "url": "/static/media/AvenirNext-BoldItalic.8ac88e91.ttf"
+  },
+  {
+    "revision": "426f464ea94cd55eb02b4c02e25bdfb4",
+    "url": "/static/media/AvenirNext-DemiBold.426f464e.ttf"
+  },
+  {
+    "revision": "c13c2a36924dc0703d69c94ace8fbbc7",
+    "url": "/static/media/AvenirNext-DemiBoldItalic.c13c2a36.ttf"
+  },
+  {
+    "revision": "f3340fbd9f3f2fe12bf4b1aa0e31c7fa",
+    "url": "/static/media/AvenirNext-Heavy.f3340fbd.ttf"
+  },
+  {
+    "revision": "d21c3e2b0fea3eb8eab3219ee6392b26",
+    "url": "/static/media/AvenirNext-HeavyItalic.d21c3e2b.ttf"
+  },
+  {
+    "revision": "806fd9ebe0b4a428ae849c7e75b284b1",
+    "url": "/static/media/AvenirNext-Italic.806fd9eb.ttf"
+  },
+  {
+    "revision": "45f91528e885ed651dc4b9525eb43d47",
+    "url": "/static/media/AvenirNext-Medium.45f91528.ttf"
+  },
+  {
+    "revision": "4e9d0bbe2881a7be0df6800f1923405f",
+    "url": "/static/media/AvenirNext-MediumItalic.4e9d0bbe.ttf"
+  },
+  {
+    "revision": "8b3707a381d253fe8d40e870f0aa8864",
+    "url": "/static/media/AvenirNext-Regular.8b3707a3.ttf"
+  },
+  {
+    "revision": "2688aeda9413c417c0b14e38fcf4b80b",
+    "url": "/static/media/AvenirNext-UltraLight.2688aeda.ttf"
+  },
+  {
+    "revision": "3bb556b6e518f0d67ec9422e5b2f56cc",
+    "url": "/static/media/AvenirNext-UltraLightItalic.3bb556b6.ttf"
+  },
+  {
+    "revision": "f93b93ebc3b339a64c278c8463fcf280",
+    "url": "/static/media/add.f93b93eb.svg"
+  },
+  {
+    "revision": "8ab55a6ce129eea6a9298febc54b004f",
+    "url": "/static/media/arrow-right.8ab55a6c.svg"
+  },
+  {
+    "revision": "61128a1459d5bebbb5437548c0571a49",
+    "url": "/static/media/basics.61128a14.svg"
+  },
+  {
+    "revision": "65081cf4d0c1f5c4ac1809f5b3983f2a",
+    "url": "/static/media/drop_file.65081cf4.svg"
+  },
+  {
+    "revision": "fd1a578c56653fa76b81fc936eb95f85",
+    "url": "/static/media/edit.fd1a578c.svg"
+  },
+  {
+    "revision": "be387da1ca1d63a605feb52bb4782620",
+    "url": "/static/media/experiences.be387da1.svg"
+  },
+  {
+    "revision": "7c6a3e38b19d5591217d159d9feae3f2",
+    "url": "/static/media/eye.7c6a3e38.svg"
+  },
+  {
+    "revision": "66b523aaf7ab67f63442e802874c6f19",
+    "url": "/static/media/facebook.66b523aa.svg"
+  },
+  {
+    "revision": "7ebc2e3be77652633c91941f79534562",
+    "url": "/static/media/github.7ebc2e3b.svg"
+  },
+  {
+    "revision": "44e02920f3eaf5eb4486f138ba17a019",
+    "url": "/static/media/hobbies.44e02920.svg"
+  },
+  {
+    "revision": "06ecb09531d7c1b40854403f2c9b0363",
+    "url": "/static/media/home.06ecb095.svg"
+  },
+  {
+    "revision": "25e3f36dcec278bd8c8e3ddfaa396b99",
+    "url": "/static/media/interested_by.25e3f36d.svg"
+  },
+  {
+    "revision": "7971a1d66a06c3b27cae5e28ecff8c9b",
+    "url": "/static/media/keyboard_arrow_down.7971a1d6.svg"
+  },
+  {
+    "revision": "cf4e6acc6cb683dddab285f61fe7975c",
+    "url": "/static/media/languages.cf4e6acc.svg"
+  },
+  {
+    "revision": "4d0d8424d81c4651aa7ba29d5cca5559",
+    "url": "/static/media/link.4d0d8424.svg"
+  },
+  {
+    "revision": "bd070bc90a78eb8ada4e14ec8f5654e0",
+    "url": "/static/media/linkedin.bd070bc9.svg"
+  },
+  {
+    "revision": "5d71b6c6d5da6623307ab5c08e64a11e",
+    "url": "/static/media/location.5d71b6c6.svg"
+  },
+  {
+    "revision": "fbb0908c84d21db87deec394d98edb1e",
+    "url": "/static/media/location.fbb0908c.svg"
+  },
+  {
+    "revision": "98c032aa5b949c5b0ce66072468da757",
+    "url": "/static/media/move.98c032aa.svg"
+  },
+  {
+    "revision": "33ca1c329718ba2688303f981ab15443",
+    "url": "/static/media/move_list.33ca1c32.svg"
+  },
+  {
+    "revision": "04d6f20e5cc96c6887eda91696046b1d",
+    "url": "/static/media/photo_camera.04d6f20e.svg"
+  },
+  {
+    "revision": "d796fbf48b30ad5c4819d0e51c53f58b",
+    "url": "/static/media/projects.d796fbf4.svg"
+  },
+  {
+    "revision": "d6a1f672c4cdfef75bab7a2820cf5061",
+    "url": "/static/media/remove_circle.d6a1f672.svg"
+  },
+  {
+    "revision": "5077191677092c8e09df4bd2329b86c0",
+    "url": "/static/media/school.50771916.svg"
+  },
+  {
+    "revision": "c178c937ab5f8f35c5e14cd982b28d56",
+    "url": "/static/media/school.c178c937.svg"
+  },
+  {
+    "revision": "8beab66e7aa906280d09713362e4745e",
+    "url": "/static/media/share.8beab66e.svg"
+  },
+  {
+    "revision": "e6fdbdda732c3dee1c52c7bc3f9dc83d",
+    "url": "/static/media/skills.e6fdbdda.svg"
+  },
+  {
+    "revision": "51e7ebaefce815cb5f948061dde23c5e",
+    "url": "/static/media/soundtrack.51e7ebae.svg"
+  },
+  {
+    "revision": "9477f485cec8236d40052d50d2469149",
+    "url": "/static/media/spotify.9477f485.svg"
+  },
+  {
+    "revision": "7cfc0eca90f6e1537597b58ec90a8b2c",
+    "url": "/static/media/success.7cfc0eca.svg"
+  },
+  {
+    "revision": "0107ee21f69f77499bf66f86b672d49e",
+    "url": "/static/media/trash.0107ee21.svg"
+  },
+  {
+    "revision": "17282e5ae8bd1da254cb1916a9acdaaa",
+    "url": "/static/media/twitter.17282e5a.svg"
+  },
+  {
+    "revision": "f32945c68c7d3cdc7ea246fcc414b4b8",
+    "url": "/static/media/warn.f32945c6.svg"
+  },
+  {
+    "revision": "21e1fe056e44ca40bcd967f5fa9134a3",
+    "url": "/static/media/welovedevs.21e1fe05.svg"
+  }
+]);
